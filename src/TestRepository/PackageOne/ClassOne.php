d62e4935a15e1b9ce434119d30247ea99a12c80a
bmvzane <bmvzane@users.noreply.github.com>
2016-08-01 13:27:12 -0700

    Added class
    Added class

---
+++ src/TestRepository/PackageOne/ClassOne.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TestRepository\PackageOne;
+
+class ClassOne
+{
+
+    public function __construct()
+    {
+    
+    }
+    
+    public function methodOne()
+    {
+        printf('%s::%s has been called!', __CLASS__, __METHOD__);
+    }
+
+}
